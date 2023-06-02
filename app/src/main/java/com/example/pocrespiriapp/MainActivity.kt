7acab0b89e6ebb6ddbfc51abcb53fa57e206c8e9
Joseph Stephen Chua <joseph.chua@respiri.co>
2023-06-02 13:11:06 +0800

    java 8 to java 11

--- app/src/main/java/com/example/pocrespiriapp/MainActivity.kt
+++ app/src/main/java/com/example/pocrespiriapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.pocrespiriapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mylibrary.AlgorithmLogic
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AlgorithmLogic().logic(this, "Test logic")
+
     }
 }
