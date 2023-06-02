4efc7a7e0f92893033f0a7643b1ae546b3536c84
Joseph Stephen Chua <joseph.chua@respiri.co>
2023-06-02 09:39:02 +0800

    Initial Commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/AlgorithmLogic.kt
@@ -0,0 +1,12 @@
+package com.example.mylibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class AlgorithmLogic {
+
+    fun logic(context: Context, input: String) {
+        Toast.makeText(context, input, Toast.LENGTH_LONG).show()
+    }
+
+}
